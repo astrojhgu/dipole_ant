57c311f255463ffe53293db49e90f65511479964
GU Junhua <jhgu@nao.cas.cn>
2021-12-28 18:57:04 +0800

    modified:   src/free_dipole.rs 	modified:   src/lib.rs

--- src/free_dipole.rs
+++ src/free_dipole.rs
@@ -9,7 +9,7 @@ use num::{
 type Ftype=f64;
 
 
-pub fn e_theta(theta:Ftype, l_over_lambda: f64)->Ftype{
+pub fn e_theta(theta:Ftype, l_over_lambda: Ftype)->Ftype{
     let kl=2.0*Ftype::PI()*l_over_lambda;
     let st=theta.sin();
     if st==Ftype::zero(){
@@ -17,4 +17,13 @@ pub fn e_theta(theta:Ftype, l_over_lambda: f64)->Ftype{
     }else{
         ((kl/2.0*theta.cos()).cos()-(kl/2.0).cos())/st
     }    
+}
+
+pub fn stokes(theta: Ftype, l_over_lambda: Ftype)->(Ftype, Ftype, Ftype){
+    let exx=e_theta(theta, l_over_lambda).powi(2);
+    let eyy=0.0;
+    let stokes_I=exx+eyy;
+    let stokes_Q=exx-eyy;
+    let stokes_U=exx.sqrt()*eyy.sqrt()*1.0;
+    (stokes_I, stokes_Q, stokes_U)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,2 @@
 pub mod free_dipole;
+pub mod utils;
